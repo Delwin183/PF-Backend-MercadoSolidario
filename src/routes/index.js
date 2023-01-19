0b79383ffaff1bef0e0b57f8c214cdb19b0c39ea
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-19 18:02:29 -0300

    refactor&fix(routes and controllers): repair route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
 const ongRouter = require('./ONGs/ongRouter');
+const postsRouter = require('./PostsONG/postsRouter');
 
 const router = Router();
 
 // router.use('/volunteers')
 router.use('/ongs', ongRouter)
 // router.use('/companies')
+router.use('/ongs', postsRouter)
 
 module.exports = router;
