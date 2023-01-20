12c598d88024f15f27fe9c3e55d55654dcdd637d
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-20 16:23:07 -0300

    feat(routes/post): add route posts for id

--- src/routes/PostsONG/postsRouter.js
+++ src/routes/PostsONG/postsRouter.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const {getPosts, createPost} = require('../../controllers/PostsControllers/postsController')
+const {getPosts, createPost, getPostsForId} = require('../../controllers/PostsControllers/postsController')
 
 router.get("/posts", async (req, res) => {
     const posts = await getPosts(req.body);
     res.status(200).send(posts);
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await getPostsForId(req.params.id);
+        res.status(200).send(post);
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
 })
 
 router.post("/newpost", async (req, res) => {
