2906b140644acb2dc21c22a614a9e97b69df8775
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-21 14:36:25 -0300

    fix(routes users): add try catch

--- src/routes/Users/usersRouter.js
+++ src/routes/Users/usersRouter.js
@@ -8,13 +8,17 @@ router.post("/newuser", async (req, res) => {
         const newUser = await signUp(req.body);
         res.status(200).send(newUser);
     } catch (error) {
-        
+        res.status(400).send(error.message)
     }
 })
 
-router.get("/getusers", async (req, res) => {
-    const allUsers = await getUsers()
-    res.status(200).send(allUsers);
+router.get("/", async (req, res) => {
+    try {
+        const allUsers = await getUsers();
+        res.status(200).send(allUsers);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 })
 
 module.exports = router;
