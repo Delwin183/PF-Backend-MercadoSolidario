927ab652295be7866bc950c8e79c90c978c62aeb
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-21 14:31:23 -0300

    fix(controller and route): add index router

--- src/routes/Users/usersRouter.js
+++ src/routes/Users/usersRouter.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const {signUp, getUsers} = require('../../controllers/UsersControllers/usersControllers')
 
 router.post("/newuser", async (req, res) => {
-    const newUser = await signUp(req.body);
-    res.status(200).send(newUser);
+    try {
+        const newUser = await signUp(req.body);
+        res.status(200).send(newUser);
+    } catch (error) {
+        
+    }
 })
 
 router.get("/getusers", async (req, res) => {
