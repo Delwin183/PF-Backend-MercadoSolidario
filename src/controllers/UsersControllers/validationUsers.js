e876aeb61c9877833aab2318bb81b663cc31a8aa
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-21 16:52:24 -0300

    fix(validation users): update ifs and returns

--- src/controllers/UsersControllers/validationUsers.js
+++ src/controllers/UsersControllers/validationUsers.js
@@ -2,32 +2,35 @@ function validateUser(body){
     const {name, lastName, cuil, user_linkedin, profession, email, password} = body;
 
     if(!name) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese el nombre.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese el nombre.'};
     }
 
     if(!lastName) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese el apellido.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese el apellido.'};
     }
 
     if(!cuil) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese el CUIL.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese el CUIL.'};
     }
 
     if(!user_linkedin) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese su usuario de LinkedIn.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese su usuario de LinkedIn.'};
     }
 
     if(!profession) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese su profesión.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese su profesión.'};
     }
 
     if(!email) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese el email.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese el email.'};
     }
 
     if(!password) {
-        return {containErrors: true, message: ('Este campo es requerido, por favor ingrese su contraseña.')};
+        return {containErrors: true, message: 'Este campo es requerido, por favor ingrese su contraseña.'};
     }
+
+    return {containErrors: false, message: 'Usted se registró correctamente, muchas gracias.'};
+
 }
 
 module.exports = validateUser;
