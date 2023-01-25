927286ae1f5c88d19498d419d12f1fc345893cc9
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-01-25 16:30:37 -0300

    fix(controller): update validation register

--- src/controllers/Auth/registrationController.js
+++ src/controllers/Auth/registrationController.js
@@ -1,12 +1,10 @@
 const prisma = require("../../db");
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { SECRET_KEY_JWT } = process.env;
 
 module.exports = {
     registerOfAUser: async function (body) {
       let {email, password, id} = body;
-      const user = await prisma[`${type_of_user}`].findUnique({where: {id}})
+      const user = await prisma.users.findUnique({where: {id}})
       
       if (user) {
         throw new Error({containErrors: true, message: 'Ya existe un usuario con este email'})
