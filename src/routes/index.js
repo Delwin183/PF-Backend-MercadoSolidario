4a7bdf8fa303a1ba268e5c03b75ef9e89dfb3163
Delwin183 <90370429+Delwin183@users.noreply.github.com>
2023-01-29 11:15:29 -0400

    fix/allusers routes names change

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const companyRouter = require("./Companies/companiesRouter");
 const usersRouter = require("./Users/usersRouter");
 const confirmedRouter = require("./Confirmed/confirmedRouter");
 const loginRouter = require("./AuthRoutes/loginRouter");
-// const allUsersrouter = require("./AllUserTypesRoutes/AllUserTypesRoute");
+const allUsersrouter = require("./AllUsersRouter/allUsersRouter");
 
 const router = Router();
 
@@ -17,6 +17,6 @@ router.use("/company", companyRouter);
 router.use("/user", usersRouter);
 router.use("/confirmed", confirmedRouter);
 router.use("/login", loginRouter);
-// router.use("/allusers", allUsersrouter);
+router.use("/allusers", allUsersrouter);
 
 module.exports = router;
